7a919b27af6ff2b01783039e86d27e2cbe17beed
ashish937200 <92686750+ashish937200@users.noreply.github.com>
2021-10-17 22:42:21 +0530

    Create stringCalculator.java

---
+++ src/stringCalculator.java
@@ -0,0 +1,10 @@
+  @Test
+    void empty_string_should_return_0(){
+        StringCalculatorShould s1 = new StringCalculator();
+        assertEquals(0, StringCalculator.add(""));
+
+    }
+    @Test
+    void string_with_single_number_should_return_number_as_int(){
+        StringCalculatorShould s1 = new StringCalculator();
+        assertEquals(0, StringCalculator.add("1"));
